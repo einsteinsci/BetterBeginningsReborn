26786f985c90f4aab202903dbe0f4ef6ff1597b3
sealedinterface <sealedinterface@gmail.com>
2017-11-02 14:05:55 -0700

    Removed duplicates in .gitignore

--- src/main/java/net/einsteinsci/betterbeginnings/inventory/slots/SlotOutput.java
+++ src/main/java/net/einsteinsci/betterbeginnings/inventory/slots/SlotOutput.java
@@ -8,7 +8,7 @@ import net.minecraftforge.items.SlotItemHandler;
 
 public class SlotOutput extends SlotItemHandler 
 {	
-	public SlotOutput(IItemHandler itemHandler, int index, int xPosition, int yPosition) 
+	public SlotOutput(IItemHandler itemHandler, int index, int xPosition, int yPosition)
 	{
 		super(itemHandler, index, xPosition, yPosition);
 	}
